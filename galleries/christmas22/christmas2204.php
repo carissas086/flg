c539bb462e88ac5dc03e74e19842dc92eb3ce6bf
EunhyeJang <carissaso@gmail.com>
2022-10-25 05:16:04 -0700

    fix gallery and added controls
    updated jquery, added functional controls for gallery aside population

---
+++ galleries/christmas22/christmas2204.php
@@ -0,0 +1,38 @@
+<div class="grid-item">
+
+<!-- ==== UNIQUE ID CALL ==== -->
+    <a href="#" aria-controls="aside" aria-expanded="false"
+        class="tile" id="flg4"
+    >
+        <!-- ==== GALLERY IMAGE THUMBNAIL ==== -->
+            <img src="images/eh-noel.png">
+
+        <!-- ==== ASIDE INFORMATION ==== -->
+            <div class="visually-hidden">
+
+            <!-- ==== FULL RESOLUTION IMAGE/CONTENT ==== -->
+                <!-- styled in aside -->
+                <div class="aside-img">
+                    <img src="images/eh-noel.png">
+                </div>
+
+            <!-- ==== WRITTEN DESCRIPTION ==== -->
+                <div class="details panel-box">
+                    <div class="aside-text">
+
+                    <!-- ==== PIECE NAME ==== -->
+                        <h3 class="head-font christmas22-header">Tester Image 2</h3>
+
+                    <!-- ==== AUTHOR NAME  |  MEDIUM ==== -->
+                        <p class="byline christmas22-byline">by: Little Bright Lee  &verbar;  <span class="italics bold">Sculpture</span></p>
+
+                    <!-- ==== BLURB ==== -->
+                        <p>This is a tester paragraph. I will fill this with filler text for a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sodales augue ligula, at facilisis felis vestibulum sit amet. Nullam auctor magna ut turpis lacinia semper. Integer nec orci blandit, molestie arcu id, sagittis est.</p>
+                        <p>This is a second tester paragraph. I will fill this with more filler text for a second paragraph. Sed finibus porta vulputate. Integer vulputate, nibh vitae consequat fringilla, elit mauris malesuada odio, a dapibus est neque in enim. Integer pulvinar ex condimentum condimentum feugiat. Nunc efficitur dolor tristique, pretium augue nec, efficitur dui. Praesent tristique dapibus eros, ac molestie quam. Praesent hendrerit consectetur sapien, quis varius elit volutpat sed.</p>
+                        
+<!-- ==== FIN ==== -->
+                    </div>
+                </div>
+            </div>
+    </a>
+</div>
--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,18 @@
-var $parent = $("#main"),
+var $parent = $('#gallery'),
 	$aside = $("#panel"),
 	$asideTarget = $aside.find(".aside--details"),
+	$asideImgTarget = $aside.find(".aside--image"),
 	$asideClose = $aside.find(".close"),
 	$tilesParent = $(".grid"),
-	$tiles = $tilesParent.find("a"),
+	$tiles = $tilesParent.find(".tile"),
+	cntlNext = document.querySelector(".cntl-next"),
+	cntlPrev = document.querySelector(".cntl-prev"),
 	slideClass = "show-detail";
 
 // tile click
 $tiles.on("click", function (e) {
+	setControlIds(this);
+
 	e.preventDefault();
 	e.stopPropagation();
 	if (!$("html").hasClass(slideClass)) {
@@ -20,19 +25,75 @@ $tiles.on("click", function (e) {
 		killAside();
 		$(this).attr("aria-expanded", "false");
 	}
+
+	// ==== puts aside to the top
+	scrollaside();
 });
+
+// gallery control funtion
+$(".gallery-control").on("click", function () {
+	const thisId = this.getAttribute('data-id');
+	setControlIds(this);
+
+	$asideTarget.html("");
+	loadTileData($("#" + thisId));
+});
+
+// set control function targets
+function setControlIds(target) {
+
+	let curr;
+	let thisId = target.getAttribute('data-id');
+
+	if (thisId == null) {
+		curr = target.getAttribute('id');
+	} else {
+		curr = thisId;
+	}
+
+	var setId = curr.replace("flg","");
+	
+	// ==== sets current id +1
+		var setNext = Number(setId) + 1;
+	// ==== sets current id -1
+		var setPrev = Number(setId) - 1;
+		
+	// ==== STOP AT MIN GALLERY ITEMS
+	if (curr == '1') {
+		cntlPrev.setAttribute('data-id','flg1');
+		cntlNext.setAttribute('data-id','flg2');
+		return;
+	} else {
+		cntlPrev.setAttribute('data-id','flg' + setPrev.toString());
+	};
+
+	// ==== SET MAX GALLERY ITEMS
+	if(curr == '4') {
+		cntlPrev.setAttribute('data-id','flg' + setPrev.toString());
+		cntlNext.setAttribute('data-id',thisId);
+		return;
+	} else {
+		cntlNext.setAttribute('data-id','flg' + setNext.toString());
+	};
+};
 
 // kill aside
 $asideClose.on("click", function (e) {
 	e.preventDefault();
+	$asideTarget.html("");
+	$asideImgTarget.html("");
 	killAside();
 });
 
 // load data to aside
 function loadTileData(target) {
 	var $this = $(target),
-		itemHtml = $this.find(".details").html();
+
+	itemHtml = $this.find(".aside-img").html();
+	$asideImgTarget.html(itemHtml);
+	itemHtml = $this.find(".details").html();
 	$asideTarget.html(itemHtml);
+
 	showAside();
 }
 
@@ -60,15 +121,24 @@ window.addEventListener(
 	false
 );
 
+// handle body click to close off-canvas
+$parent.on("click", function (e) {
+	if ($("html").hasClass(slideClass)) {
+		e.preventDefault();
+		e.stopPropagation();
+		killAside();
+	}
+});
+
 // kill aside
 function killAside() {
+
 	if ($("html").hasClass(slideClass)) {
 		$("html").removeClass(slideClass);
 		$aside.removeClass("visible");
 		sendFocusBack();
 		$aside.attr("aria-hidden", "true");
 		$tiles.attr("aria-expanded", "false");
-
 	}
 }
 
@@ -82,9 +152,13 @@ function sendFocusBack() {
 	$(".active").focus();
 }
 
-// handle body click to close off-canvas
-$parent.on("click", function (e) {
-	if ($("html").hasClass(slideClass)) {
-		killAside();
-	}
+// back to top link
+$("#toplink").on("click", function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	scrollaside();
 });
+
+function scrollaside () {
+	document.querySelector('#panel').scrollTo(0,0);
+}
