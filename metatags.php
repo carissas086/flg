c03571638ec8cbde3a4d5bb2c78a4998ca5617ce
carissas <92591984+carissas086@users.noreply.github.com>
2022-12-02 00:14:21 -0800

    hot fix metatags update

--- metatags.php
+++ metatags.php
@@ -1,8 +1,8 @@
 <meta property="og:site_name" content="Fraser Lands Gallery" />
 <meta property="og:title" content="Fraser Lands Gallery" />
-<meta property="og:url" content="https://flg.carissaso.ca"/>
+<meta property="og:url" content="https://fraserlandsgallery.com"/>
 <meta property="og:type" content="website" />
 <meta property="og:description" content="Visit the Fraser Lands Gallery online. 'A Christmas Art Exhibit' is now open, come and take a look!" />
 <meta property="og:image" content="/images/christmas22_meta-banner.jpg" />
-<meta property="og:image:width" content="1140" />
-<meta property="og:image:height" content="350" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="675" />
