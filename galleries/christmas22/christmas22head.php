cd08865700104819d1782fac8d653e72f1ad4080
EunhyeJang <carissaso@gmail.com>
2022-12-01 16:16:47 -0800

    head section
    content population, template setup, styling

--- galleries/christmas22/christmas22head.php
+++ galleries/christmas22/christmas22head.php
@@ -3,11 +3,8 @@
     <div class="gallery-header is-flex flex-centers christmass22-header">
         <h4 class="head-font">Fraser Lands Gallery Presents:</h4>
         <h1 class="christmas22-title">A Christmas Art Exhibit</h1>
-        <div class="verse-box">
-            <p class="verse"><sup>12 </sup>This is a tester verse paragraph, because I imagine this will happen.</p>
-            <p class="verse"><sup>13 </sup>Greator love has no one than this: to lay down one's life for one's friends.</p>
-            <p class="ref">John 15:13 [NIV]</p>
-        </div>
+        <h2>Nativity</h2>
+        <p>The word "nativity" derives from the latin word, "nativitas" which means "birth." In Christian tradition "nativity" refers to the birth of Jesus Christ, both the arrival of Jesus and the miraculous circumstances. For centuries people have depicted the Nativity scene in Bethlehem in various forms: from wooden models, to paintings, to theatre dramatizations. The depiction of this scene draws us into the wonder of Christmas. This practice dates back to Francis of Assisi, who created a live action representation of the manger scene in 1223. In the bible story each of the nativity characters goes on a unique journey. What calls and draws them to the manger depends on their role and relationship to Jesus. Each of these journeys gives us a glimpse into the joy of Christmas.</p>
     </div>
     <a href="#galleryhd" class="button">
         <p>View Gallery</p>
