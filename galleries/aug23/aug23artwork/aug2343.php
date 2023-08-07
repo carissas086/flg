5eafc7b944360665f00009571912a14d3eae4fba
carissas086 <carissaso@gmail.com>
2023-08-06 17:45:47 -0700

    flg43 upload
    blurb still missing...

--- galleries/aug23/aug23artwork/aug2343.php
+++ galleries/aug23/aug23artwork/aug2343.php
@@ -1,5 +1,5 @@
 <!-- FLG43 -->
-<div class="grid-item g-aug23 m-arcrylic m-painting a-006">
+<div class="grid-item g-aug23 m-photograph a-020">
     <a href="#" aria-controls="aside" aria-expanded="false"
         class="tile video" id="flg43">
         <img src="images/flg43_thumb.jpg" alt="">
@@ -13,12 +13,12 @@
                 </div>
                 <div class="details panel-box is-flex">
                     <div class="aside-text">
-                        <h3 class="head-font aside-header">Embraced by God&apos;s Love</h3>
-                        <p class="medium italics bold">Acrylic Painting</p>
-                        <p>Only Jesus is the absolute practitioner of God&apos;s covenant love.</br>Under His grace and mercy, we bring our brokenness to Him for cleansing and healing.</br>We begin learning to live out His love by following and walking towards Him with the people around us while</br>surrounded by His covenant love.</p>
+                        <h3 class="head-font aside-header"></h3>
+                        <p class="medium italics bold">Photograph</p>
+                        <!-- <p>XX</p> -->
                         <div class="is-flex bio-box">
-                            <img src="images/Clara-Yeung_2211.jpg" alt="Photo of Clara Yeung">
-                            <p class="byline">by: Clara Yeung-Leong</p>
+                            <img src="images/Pearl-Chan_2211.jpg" alt="Photo of Pearl Chan">
+                            <p class="byline">by: Pearl Chan</p>
                         </div>
                     </div>
                 </div>
--- galleries/aug23/aug23artwork/aug2347.php
+++ galleries/aug23/aug23artwork/aug2347.php
@@ -1,5 +1,5 @@
 <!-- FLG47 -->
-<div class="grid-item g-aug23 m-photograph a-020">
+<div class="grid-item g-aug23 m-arcrylic m-painting a-006">
     <a href="#" aria-controls="aside" aria-expanded="false"
         class="tile video" id="flg47">
         <img src="images/flg47_thumb.jpg" alt="">
@@ -13,12 +13,12 @@
                 </div>
                 <div class="details panel-box is-flex">
                     <div class="aside-text">
-                        <h3 class="head-font aside-header"></h3>
-                        <p class="medium italics bold">Photograph</p>
-                        <p>XX</p>
+                        <h3 class="head-font aside-header">Embraced by God&apos;s Love</h3>
+                        <p class="medium italics bold">Acrylic Painting</p>
+                        <p>Only Jesus is the absolute practitioner of God&apos;s covenant love.</br>Under His grace and mercy, we bring our brokenness to Him for cleansing and healing.</br>We begin learning to live out His love by following and walking towards Him with the people around us while</br>surrounded by His covenant love.</p>
                         <div class="is-flex bio-box">
-                            <img src="images/Pearl-Chan_2211.jpg" alt="Photo of Pearl Chan">
-                            <p class="byline">by: Pearl Chan</p>
+                            <img src="images/Clara-Yeung_2211.jpg" alt="Photo of Clara Yeung">
+                            <p class="byline">by: Clara Yeung-Leong</p>
                         </div>
                     </div>
                 </div>
