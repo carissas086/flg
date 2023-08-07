391717d7b68a42dacc4154cda8fb6a9f3ca4b837
carissas086 <carissaso@gmail.com>
2023-08-06 18:04:13 -0700

    flg43 blurb

--- galleries/aug23/aug23artwork/aug2343.php
+++ galleries/aug23/aug23artwork/aug2343.php
@@ -15,7 +15,13 @@
                     <div class="aside-text">
                         <h3 class="head-font aside-header"></h3>
                         <p class="medium italics bold">Photograph</p>
-                        <!-- <p>XX</p> -->
+                        <div class="verse-box">
+                            <p class="verse"><sup>16 </sup>But Ruth replied, &ldquo;Don&apos;t urge me to leave you or to turn back from you. Where you go I will go, and where you stay I will stay. Your people will be my people and your God my God.</p>
+                            <p class="verse"><sup>17 </sup>Where you die I will die, and there I will be buried. May the Lord deal with me, be it ever so severely, if even death separates you and me.&rdquo;</p>
+                            <p class="ref">Ruth 1:16&ndash;17 [NIV]</p>
+                        </div>
+                        <p>Ruth&apos;s steadfast mind and love was reflected in the way she was holding Naomi&apos;s hands. It also reminds me Our Heavenly Father never gives up on us in the same way.</p>
+                        <p>Thank you Ling Po, Lemon and 007 for your help with that photo project.</p>
                         <div class="is-flex bio-box">
                             <img src="images/Pearl-Chan_2211.jpg" alt="Photo of Pearl Chan">
                             <p class="byline">by: Pearl Chan</p>
