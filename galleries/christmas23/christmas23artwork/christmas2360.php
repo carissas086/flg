e2502a58bdcf3f26b6658be9bb943ef8c6446ecd
carissas086 <carissaso@gmail.com>
2023-12-02 01:17:57 -0800

    flg 58-64, and metadata
    updates to functions and readme, and filters

---
+++ galleries/christmas23/christmas23artwork/christmas2360.php
@@ -0,0 +1,37 @@
+<!-- FLG60 -->
+<div class="grid-item g-christmas23 m-arcrylic m-sculpture m-mixedmedia a-006">
+    <a href="#" aria-controls="aside" aria-expanded="false"
+        class="tile video" id="flg60">
+        <img src="images/flg60_thumb.jpg" alt="Photograph of a paper model sculpture">
+
+        <!-- ==== FLG60 ASIDE ==== -->
+            <div class="visually-hidden">
+                <div class="aside-img">
+                    <div class="aside-top">
+                        <img src="images/flg60.jpg" alt="Photograph of a paper model sculpture">
+                    </div>
+                </div>
+                <div class="details panel-box is-flex">
+                    <div class="aside-text">
+                        <!-- <h3 class="head-font aside-header">Reflection on Magnificat</h3> -->
+                        <p class="medium italics bold">Acrylic Painting on Paper Model</p>
+                        <div class="verse-box">
+                            <p class="verse"><sup>46 </sup>And Mary Said:<br>My soul glorifies the Lord</p>
+                            <p class="verse"><sup>47 </sup>and my spirit rejoices in God my Savior,</p>
+                            <p class="verse"><sup>48 </sup>for He has been mindful<br>of the humble state of His servant.<br>From now on all generations will call me blessed,</p>
+                            <p class="verse"><sup>49 </sup>for the Mighty One has done great things for me&mdash;<br>holy is His name</p>
+                            <p class="verse"><sup>50 </sup>fHis mercy extends to those who fear Him&hellip;</p>
+                            <p class="ref">Luke 1:46&ndash;50 [NIV]</p>
+                        </div>
+                        <p>Through Jesus comes &ldquo;Joy to the world&rdquo;: &ldquo;Let Earth receive her King. Let every heart prepare Him room.&rdquo; If everyone receives Jesus as the King and has room for Him, then people will walk in the Light &mdash; &ldquo;Global Magnificat&rdquo; in Christ the Saviour!</p>
+                        <div class="is-flex bio-box">
+                            <img src="images/Clara-Yeung_2211.jpg" alt="Photo of Clara Yeung-Leong">
+                            <p class="byline">by: Clara Yeung-Leong</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- ==== FIN ==== -->
+
+    </a>
+</div>
