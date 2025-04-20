a07d9c658a6b4004021bcfa2517fc18bf1b157bc
carissas086 <carissaso@gmail.com>
2025-04-19 23:24:42 -0700

    reorder easter25 grid

--- galleries/easter25/easter25grid.php
+++ galleries/easter25/easter25grid.php
@@ -1,8 +1,8 @@
 <!-- ==== easter 25 GALLERY ARTWORKS ==== -->
 
     <?php include __DIR__ . '/easter25artwork/easter2586.php'; ?>
-    <?php include __DIR__ . '/easter25artwork/easter2587.php'; ?>
-    <?php include __DIR__ . '/easter25artwork/easter2588.php'; ?>
+    <?php include __DIR__ . '/easter25artwork/easter2591.php'; ?>
     <?php include __DIR__ . '/easter25artwork/easter2589.php'; ?>
     <?php include __DIR__ . '/easter25artwork/easter2590.php'; ?>
-    <?php include __DIR__ . '/easter25artwork/easter2591.php'; ?>
+    <?php include __DIR__ . '/easter25artwork/easter2587.php'; ?>
+    <?php include __DIR__ . '/easter25artwork/easter2588.php'; ?>
