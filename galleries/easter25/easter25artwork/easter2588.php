827b1c206a234725dcdb382e25d88e6e6ac1a247
carissas086 <carissaso@gmail.com>
2025-04-19 22:54:16 -0700

    easter25 blurbs, thumb upscales
    jr & buddy fellow artist images; style updates to accommodate

--- galleries/easter25/easter25artwork/easter2588.php
+++ galleries/easter25/easter25artwork/easter2588.php
@@ -13,7 +13,10 @@
                 </div>
                 <div class="details panel-box is-flex">
                     <div class="aside-text">
+                        <h3 class="head-font aside-header">Born Again to a Living Hope</h3>
                         <p class="medium italics bold">Pottery</p>
+                        <p>This artwork is accompanied by a Bible passage for re-orienting our hearts towards God&apos;s Kingdom when viewing the hatched Easter egg surrounded by palm leaves as a symbol of victory in Christ.</p>
+                        <p>You are invited to open your Bible or the paper scroll inside and read the passage &ldquo;1 Peter 1:3&dash;12&rdquo;.</p>
                         <div class="is-flex bio-box">
                             <img src="images/Clara-Yeung_2211.jpg" alt="Photo of Clara Yeung-Leong">
                             <p class="byline">by: Clara Yeung-Leong</p>
